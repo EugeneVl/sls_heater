33b8240e0cb075a15496d601b16e78c3e54a315a
EugeneVl <77906821+EugeneVl@users.noreply.github.com>
2021-01-24 14:12:59 +1000

    Update heater_config.lua

--- heater_config.lua
+++ heater_config.lua
@@ -12,9 +12,13 @@ local heater = {
             name = "Гостиная",
             sensor = "0x00158D0002D79850",
             switch = "0x842E14FFFE35A1E8", -- nil, если нет
+            only_switch = false,
             set_temp = 15,
-            hysteresis = 5
+            hysteresis = 0.5,
+            night_temp_offset = 5
         }
-    }
+    },
+    day_start = 7,
+    night_start = 23
 }
 return heater
