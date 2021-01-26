025cfaeb5fb06c35780ff6bc7b6cf7930f242608
EugeneVl <77906821+EugeneVl@users.noreply.github.com>
2021-01-26 22:20:21 +1000

    Update heater_config.lua

--- heater_config.lua
+++ heater_config.lua
@@ -1,11 +1,13 @@
 local heater = {
     switch = {
         addr = "0x00124B001EC79764",
-        force_full_power = "state_l1",
-        force_boiler_on = "state_l2",
-        force_switches_on = "state_l3",
-        full_power = "state_l7",
-        boiler_on = "state_l8"
+        states = {
+            force_full_power = "state_l1",
+            force_boiler_on = "state_l2",
+            force_switches_on = "state_l3",
+            full_power = "state_l7",
+            boiler_on = "state_l8"
+        }
     },
     rooms = {
         living_room = {
@@ -22,16 +24,16 @@ local heater = {
             sensor = "0x00158D0001E04E71",
             switch = "0x588E81FFFEDBCFCE",
             switch_only = true,
-            set_temp = 0,
-            hysteresis = 1,
-            night_temp_offset = 2
+            set_temp = 1,
+            hysteresis = 0.5,
+            night_temp_offset = 3
         },
         boiler_room = {
             name = "Котельная",
             sensor = "0x00158D00045C3041",
             switch = nil,
             switch_only = false,
-            set_temp = 6,
+            set_temp = 5,
             hysteresis = 1,
             night_temp_offset = 0
         }
