c9e224d44079547071c19a5b21b655418800466d
EugeneVl <77906821+EugeneVl@users.noreply.github.com>
2021-03-12 14:37:55 +1000

    Update heater_config.lua

--- heater_config.lua
+++ heater_config.lua
@@ -15,27 +15,27 @@ local heater = {
             sensor = "0x00158D0002D79850",
             switch = "0x842E14FFFE35A1E8",
             switch_only = false,
-            set_temp = 15,
-            hysteresis = 1,
-            night_temp_offset = 5
+            day_temp   = 18,
+            night_temp = 24,
+            hysteresis = 1
         },
         garage = {
             name = "Гараж",
             sensor = "0x00158D0001E04E71",
             switch = "0x588E81FFFEDBCFCE",
             switch_only = true,
-            set_temp = 1,
-            hysteresis = 0.5,
-            night_temp_offset = 3
+            day_temp   = 1,
+            night_temp = 10,
+            hysteresis = 1
         },
         boiler_room = {
             name = "Котельная",
             sensor = "0x00158D00045C3041",
             switch = nil,
             switch_only = false,
-            set_temp = 5,
-            hysteresis = 1,
-            night_temp_offset = 0
+            day_temp   = 5,
+            night_temp = 5,
+            hysteresis = 1
         }
     },
     day_starts_at = 7,
